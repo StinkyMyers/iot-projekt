35f60041497d57e9808b13be830e5a84cd6b2b7d
CMerien <marian.susina@gmail.com>
2026-05-26 10:31:23 +0200

    Uprava config.h - verejna IP adresa

--- shared/config.h
+++ shared/config.h
@@ -1,21 +1,23 @@
 // ============================================================
 // config.h — Spoločná konfigurácia pre všetky uzly
 // Tento súbor je rovnaký pre uzol1, uzol2, uzol3
+// Každý uzol je si musí upraviť WiFi konfiguráciu
 // ============================================================
 
 #ifndef CONFIG_H
 #define CONFIG_H
 
-// --- WiFi ---
-#define WIFI_SSID     "d201"
-#define WIFI_PASSWORD "MechaTronyka"          
+// --- WiFi --- 
+#define WIFI_SSID     "nazov WiFi"
+#define WIFI_PASSWORD "heslo od wifi"          
 
 // --- MQTT Broker ---
-#define MQTT_HOST     "192.168.11.100"
+#define MQTT_HOST     "147.175.105.185"
 #define MQTT_PORT     1883
 #define MQTT_PASSWORD "misa"
 
 // --- Perióda merania ---
-#define MEASUREMENT_PERIOD_MS 30000   // 30 sekúnd
+#define MEASUREMENT_PERIOD_MS 30000     // 30 sekúnd - každý uzol môže 
+                                        // upraviť podla potreby senzora
 
 #endif
